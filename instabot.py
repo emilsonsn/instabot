be286effd533a7b30132f25010001da2b33274e2
emilsonsn <emilsonsn2@gmail.com>
2022-06-20 21:41:14 -0300

    leitura da base de dados

--- instabot.py
+++ instabot.py
@@ -1,3 +1,51 @@
-username = input('Digite o username que deseja usar: ')
-password = input('Digite o sua senha: ')
+import logging
+import time
+from src.config import Config
+from selenium import webdriver
+from gettext import find
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from src.config import Options
+from logging import FileHandler, StreamHandler
+from datetime import date
+from src.usuarios import Usuarios
 
+logging.basicConfig(
+    level = logging.INFO, 
+    format= "%(asctime)s::%(levelname)s::%(filename)s::%(lineno)d - %(message)s",
+    datefmt='%m/%d/%Y %I:%M:%S',
+    handlers=[FileHandler("./logs/instabot-"+str(date.today())+".log", 'a'), StreamHandler()]
+)
+MENSAGENS = [
+    Config.get_conf()['msg1'],
+    Config.get_conf()['msg2'],
+    Config.get_conf()['msg3']
+    ]
+TEMPO_ESPERA = Config.get_conf()['tempo_espera']
+USERNAME = input('Digite o username da conta: ')
+PASSWORD = input('Digite a senha da conta: ')
+
+def logar():
+    try:
+        browser = webdriver.Chrome('chromedriver', options=Options.get_options())
+        browser.get('https://instagram.com')
+        while len(browser.find_elements(By.CSS_SELECTOR, 'input[name="username"]')) < 1:
+            time.sleep(0.5)
+        browser.find_element(By.CSS_SELECTOR, 'input[name="username"]').send_keys(USERNAME)
+        browser.find_element(By.CSS_SELECTOR, 'input[name="password"]').send_keys(PASSWORD)
+        browser.find_element(By.CSS_SELECTOR, 'input[name="password"]').send_keys(Keys.ENTER)
+        esperar = 0 
+        while len(browser.find_elements(By.CSS_SELECTOR, 'img[data-testid="user-avatar"]')) < 1:
+            time.sleep(1)
+            esperar+=1
+            print('Autenticação demorando muito...') if esperar%15 == 0 else ''
+            if esperar > 20:
+                logging.warning('Não foi possível efetuar o login corretamente. Usuário:'+USERNAME)
+                exit()
+        logging.info('Login efetuado corretamente. Usuário:'+USERNAME)
+    except:
+        logging.error('Erro inesperado ao tentar logar. Usuários:'+USERNAME)
+
+def find_profile():
+    usuarios = Usuarios.get_users('sfdf')
+logar()
---
+++ src/config.py
@@ -0,0 +1,34 @@
+import json
+from pyexpat.errors import XML_ERROR_FINISHED
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+
+class Config:
+    @staticmethod
+    def get_conf():
+        with open('config.json', 'r') as config:
+            config = json.load(config)
+            TEMPO_ESPERA_MIN = config['tempo_de_espera']*1000
+            MENSAGEM1 = config['mensagens']['mensagem1']
+            MENSAGEM2 = config['mensagens']['mensagem2']
+            MENSAGEM3 = config['mensagens']['mensagem3']
+        return {
+            "tempo_espera":TEMPO_ESPERA_MIN,
+            "msg1": MENSAGEM1,
+            "msg2": MENSAGEM2,
+            "msg3": MENSAGEM3
+        }
+        
+class Options:
+    @staticmethod
+    def get_options():
+        options = webdriver.ChromeOptions()
+        options.add_argument("--window-size=700,800")
+        options.add_argument('--disable-blink-features=AutomationControlled')
+        options.add_argument('--disable-blink-features=AutomationControlled')
+        options.add_experimental_option('useAutomationExtension', False)
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_argument("disable-infobars")
+        return options
+ 
+con = Config.get_conf()
