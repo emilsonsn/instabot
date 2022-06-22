74215ec30c1ae5bc014cff47602d1af8783cd6a6
emilsonsn <emilsonsn2@gmail.com>
2022-06-22 19:26:58 -0300

    arquivos de documentação

--- src/telegrambot.py
+++ src/telegrambot.py
@@ -9,3 +9,4 @@ class BotTelegram:
         bot = telebot.TeleBot(telegram['chave_api_telegram'])
         bot.send_message(telegram['my_id_telegram'], "Ops.. Houve algum problema em uma das intâncias do seu bot do instagram.")
         bot.send_message(telegram['my_id_telegram'], mensagem)
+BotTelegram.send_message('aca')
