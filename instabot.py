5521ba16788035f3230f910dbeb952ec73e9b296
emilsonsn <emilsonsn2@gmail.com>
2022-06-23 08:38:25 -0300

    remoção do bot do telegram

--- instabot.py
+++ instabot.py
@@ -1,4 +1,5 @@
 from asyncio import sleep
+from os import EX_CANTCREAT
 import time
 from numpy import rint
 from src.config import Config
@@ -11,7 +12,6 @@ from src.perfis import Perfis
 import random
 from src.contas import Contas
 from src.log_config import LogConfig
-from src.telegrambot import BotTelegram
 from selenium.webdriver.chrome.service import Service
 
 logging = LogConfig.get_logging()
@@ -34,7 +34,7 @@ class instabot:
         try:
             self.browser.get('https://instagram.com')
             while len(self.browser.find_elements(By.CSS_SELECTOR, 'input[name="username"]')) < 1:
-                time.sleep(0.5)
+                time.sleep(1)
             self.browser.find_element(By.CSS_SELECTOR, 'input[name="username"]').send_keys(self.USERNAME)
             self.browser.find_element(By.CSS_SELECTOR, 'input[name="password"]').send_keys(self.PASSWORD)
             self.browser.find_element(By.CSS_SELECTOR, 'input[name="password"]').send_keys(Keys.ENTER)
@@ -44,54 +44,103 @@ class instabot:
                 esperar+=1
                 logging.warning('Autenticação demorando muito...') if esperar%15 == 0 else ''
                 if esperar > 20:
-                    logging.warning('Não foi possível efetuar o login corretamente. Usuário:'+self.USERNAME)
+                    logging.warning(f"Login está demorando muito. Usuário:' {self.USERNAME}")
                     self.browser.quit()
                     exit()
             logging.info('Login efetuado corretamente. Usuário:'+self.USERNAME)
+            self.tirarNotificaocao()
         except Exception as err:
-            logging.error('Erro inesperado ao tentar logar. Usuários:'+self.USERNAME+str(err))
-            self.browser.quit()
-            exit()
+            logging.error(f"Erro inesperado ao tentar logar. Usuários:{str(err)} {self.USERNAME}")
+            logging.info(f"Reiniciando bot em 2 minuto.")
+            self.restartBot()
 
+    def tirarNotificaocao(self):
+        try:    
+            self.browser.get('https://www.instagram.com/direct/inbox/')
+            while(len(self.browser.find_elements(By.XPATH,"//button[text()='Agora não']")) < 1):
+                time.sleep(1)
+            self.browser.find_element(By.XPATH,"//button[text()='Agora não']").click()
+            while(len(self.browser.find_elements(By.CSS_SELECTOR,'button')) < 1):
+                time.sleep(1)
+            self.browser.find_elements(By.CSS_SELECTOR,'button')[0].click()
+            time.sleep(1)
+            self.browser.find_elements(By.CSS_SELECTOR,'button')[0].send_keys(Keys.ESCAPE)    
+        except Exception as err:
+            logging.error(f"Erro ao tirar notificação. {str(err)} {self.USERNAME}")  
+            logging.info(f"Reiniciando bot em 2 minuto.")
+            self.restartBot()
+            
     def find_profile(self):
-        perfis = Perfis.get_perfis(self.USERNAME)
-        if(len(perfis) > 0):
-            for perfil in perfis:
-                self.INDICE_PERFIL = perfil[0]
-                self.browser.get('https://instagram.com/'+perfil[1])
-                break
-        else:
-            logging.warning("Não existe perfis pendentes para a conta. Planilha necessita de atualização para a conta: "+self.USERNAME)
-            self.sleep_for_time(10*60)
-            self.main()
-
+        try:
+            perfis = Perfis.get_perfis(self.USERNAME)
+            if(len(perfis) > 0):
+                for perfil in perfis:
+                    self.INDICE_PERFIL = perfil[0]
+                    self.NOME_PERFIL = perfil[1]
+                    break
+            else:
+                logging.warning("Não existe perfis pendentes para a conta. Planilha necessita de atualização para a conta: "+self.USERNAME)
+                self.sleep_for_time(10*60)
+                self.main()
+        except Exception as err:
+            logging.error(f"Erro ao buscar perfis para a conta:{str(err)} {self.USERNAME}")
+            logging.info(f"Reiniciando bot em 2 minuto.")
+            self.restartBot()
+            
     def random_message(self):
-        return self.MESSAGES[random.randint(0,len(self.MESSAGES)-1)]
+        try:
+            return self.MESSAGES[random.randint(0,len(self.MESSAGES)-1)]
+        except Exception as err:
+            logging.error(f"Erro ao escolher mensagem aleatoriamente. {str(err)} {self.USERNAME}")
+            logging.info(f"Reiniciando bot em 2 minuto.")
+            self.restartBot()
 
     def sleep_for_time(self,minutos):
-        logging.info("Entrando em espera... "+str(minutos/60)+" min")
-        time.sleep(int(minutos))
-
+        try:
+            logging.info("Entrando em espera... "+str(minutos/60)+" min")
+            time.sleep(int(minutos))
+        except Exception as err:
+            logging.error(f"Erro ao entrar em modo de espera. {str(err)} {self.USERNAME}")
+            logging.info(f"Reiniciando bot em 2 minuto.")
+            self.restartBot()
+            
     def send_message(self,message):
-        time.sleep(5)
-        esperar = 0
-        while(len(self.browser.find_elements(By.XPATH, "//*[text()='Enviar mensagem']")) < 1):
+        try:
+            self.browser.get('https://www.instagram.com/direct/inbox/')
+            while(len(self.browser.find_elements(By.CSS_SELECTOR,'button')) < 1):
+                time.sleep(1)
+            self.browser.find_elements(By.CSS_SELECTOR,'button')[2].click()
+            while(len(self.browser.find_elements(By.CSS_SELECTOR,'input')) < 1):
+                time.sleep(1)
+            self.browser.find_elements(By.CSS_SELECTOR,'input')[1].send_keys(self.NOME_PERFIL)        
+            time.sleep(3)
+            while(len(self.browser.find_elements(By.CSS_SELECTOR,'circle')) < 1):
+                time.sleep(1)
+            self.browser.find_elements(By.CSS_SELECTOR,'circle')[2].click()
             time.sleep(1)
-            esperar+=1
-            if esperar == 25:
-                Perfis.set_perfil_error(self.INDICE_PERFIL)
-                self.main()
-
-        self.browser.find_element(By.XPATH,"//*[text()='Enviar mensagem']").click()
-        while (len(self.browser.find_elements(By.CSS_SELECTOR, 'textarea'))<1):
-            time.sleep(2)
-        self.browser.find_element(By.CSS_SELECTOR, 'textarea').send_keys(message)
-        self.browser.find_element(By.CSS_SELECTOR, 'textarea').send_keys(Keys.ENTER)
-        logging.info("Mensagem enviada com sucesso!")
-        Perfis.set_perfil(self.INDICE_PERFIL)
-        Contas.set_count(self.USERNAME)
-        time.sleep(1)
-
+            self.browser.find_element(By.XPATH,"//div[text()='Avançar']").click()
+            while (len(self.browser.find_elements(By.CSS_SELECTOR, 'textarea'))<1):
+                time.sleep(2)
+            self.browser.find_element(By.CSS_SELECTOR, 'textarea').send_keys(message)
+            time.sleep(1)
+            self.browser.find_element(By.CSS_SELECTOR, 'textarea').send_keys(Keys.ENTER)
+            logging.info("Mensagem enviada com sucesso!")
+            Perfis.set_perfil(self.INDICE_PERFIL)
+            Contas.set_count(self.USERNAME)
+            time.sleep(1)
+        except Exception as err:
+            logging.error(f"Erro ao tentar enviar mensagem. {str(err)} {self.USERNAME}")
+            logging.info(f"Reiniciando bot em 2 minuto.")
+            self.restartBot()
+                     
+    def restartBot(self):
+            self.browser.quit()
+            self.sleep_for_time(2*60)
+            logging.info("Reiniciando sistema automáticamente...")
+            self.browser = webdriver.Chrome(service=service, options=Options.get_options())
+            self.logar()
+            self.main()        
+            
     def main(self):
         try:
             self.find_profile()
@@ -99,11 +148,7 @@ class instabot:
             self.sleep_for_time(self.TEMPO_ESPERA)
             self.main()
         except Exception as err:
-            logging.critical('Problema inesperado na execução do script. '+str(err))
-            logging.info("Reiniciando sistema em 5 minutos...")
-            BotTelegram.send_message("Mensagem de erro interna do sistema: "+str(err))
-            self.sleep_for_time(5*60)
-            self.browser.refresh()
-            self.main()
+            self.restartBot()
+            
 bot = instabot()
 bot.main()
--- src/telegrambot.py
+++ src/telegrambot.py
@@ -1,12 +1,11 @@
 from click import command
-from config import Telegram
 import telebot
 
 class BotTelegram:
     @staticmethod
-    def send_message(mensagem):
-        telegram = Telegram.get_telegram()
+    def send_message(mensagem, telegramConfig):
+        telegram = telegramConfig
         bot = telebot.TeleBot(telegram['chave_api_telegram'])
         bot.send_message(telegram['my_id_telegram'], "Ops.. Houve algum problema em uma das intâncias do seu bot do instagram.")
         bot.send_message(telegram['my_id_telegram'], mensagem)
-BotTelegram.send_message('aca')
+
